987e554e4860170caf04b1a8f6ba715dfc74aecf
congminh2812 <congminh281297@gmail.com>
2024-11-11 23:16:59 +0700

    add DDD

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,10 +1,23 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models
 {
-    public class Customer : Entity<Guid>
+    public class Customer : Entity<CustomerId>
     {
-        public string Name { get; set; } = default!;
-        public string Email { get; set; } = default!;
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        public static Customer Create(CustomerId id, string name, string email)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
