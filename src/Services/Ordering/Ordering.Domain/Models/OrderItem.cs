26db9a7df658bf8e272846bc275d2ca01ff8fa5b
congminh2812 <congminh281297@gmail.com>
2024-11-12 23:11:56 +0700

    update DDD

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,12 +1,19 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models
 {
-    public class OrderItem : Entity<Guid>
+    public class OrderItem : Entity<OrderItemId>
     {
-        public Guid OrderId { get; set; }
-        public Guid ProductId { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
+        public OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItemId.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
     }
 }
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,10 +1,23 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models
 {
-    public class Product : Entity<Guid>
+    public class Product : Entity<ProductId>
     {
-        public string Name { get; set; } = default!;
-        public decimal Price { get; set; }
+        public string Name { get; private set; } = default!;
+        public decimal Price { get; private set; }
+
+        public static Product Create(ProductId id, string name, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var product = new Product
+            {
+                Id = id,
+                Name = name,
+                Price = price
+            };
+
+            return product;
+        }
     }
 }
