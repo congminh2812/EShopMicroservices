c236b3f7d14549cc63cbc713e257d6ee20b48712
minhnc <congminh281297@gmail.com>
2025-07-28 17:19:29 +0700

    Test CI/CD pipeline of dev branch

--- src/Services/Basket/Basket.API/Data/BasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -20,6 +20,7 @@
 
         public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken)
         {
+            // Test CI/CD pipeline of dev branch
             session.Delete<ShoppingCart>(userName);
             await session.SaveChangesAsync(cancellationToken);
 
